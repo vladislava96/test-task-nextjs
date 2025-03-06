0eeaa76ee19cc9c987465c91aa8a071250797903
Vladislava <vladislavaskrabova@gmail.com>
2025-03-06 13:08:16 +0300

    feat: implement warning for slide remove

--- app/components/plan-slide/PlanSlide.tsx
+++ app/components/plan-slide/PlanSlide.tsx
@@ -4,6 +4,7 @@ import SlideForm from "../slide-form/SlideForm";
 import { Slide, slideRemoved } from "@/lib/features/generation-form/generationFormSlice";
 import { useAppDispatch } from "@/lib/hooks";
 import { Trash2 } from "lucide-react";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 
 interface PlanSlideProps {
   item: Slide
@@ -28,7 +29,18 @@ export default function PlanSlide({ item }: PlanSlideProps) {
       </CardHeader>
       <CardFooter className="flex justify-end gap-x-2">
         <SlideForm usage="edit" item={item} />
-        <Button variant="ghost" onClick={handleDelete}><Trash2 /></Button>
+        <Dialog>
+          <DialogTrigger><Button variant="ghost"><Trash2 /></Button></DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you absolutely sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your slide.
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter><Button onClick={handleDelete}>Yes, I want to delete this slide.</Button></DialogFooter>
+          </DialogContent>
+        </Dialog>
       </CardFooter>
     </Card>
   )
